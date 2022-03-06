7df8b92ea307af4227e45881b2e99e14c270dea3
bartoszstepien01 <bartoszstepien01@users.noreply.github.com>
2022-03-06 22:22:56 +0000

    deploy: 24d5a4460c6c27f570efdd2dee6fbb7eac777c29

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const timestamp = 1646605375622;
+const build = [
+  "/rubel-przelicznik/_app/start-e9782411.js",
+  "/rubel-przelicznik/_app/pages/__layout.svelte-3a849228.js",
+  "/rubel-przelicznik/_app/assets/pages/__layout.svelte-8eea9c1e.css",
+  "/rubel-przelicznik/_app/error.svelte-75234407.js",
+  "/rubel-przelicznik/_app/pages/index.svelte-c5dc1c17.js",
+  "/rubel-przelicznik/_app/chunks/vendor-cd07b5a5.js"
+];
+const files = [
+  "/rubel-przelicznik/favicon.png",
+  "/rubel-przelicznik/manifest.json"
+];
+const worker = self;
+const FILES = `cache${timestamp}`;
+const to_cache = build.concat(files);
+const staticAssets = new Set(to_cache);
+worker.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(FILES).then((cache) => cache.addAll(to_cache)).then(() => {
+    worker.skipWaiting();
+  }));
+});
+worker.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then(async (keys) => {
+    for (const key of keys) {
+      if (key !== FILES)
+        await caches.delete(key);
+    }
+    worker.clients.claim();
+  }));
+});
+async function fetchAndCache(request) {
+  const cache = await caches.open(`offline${timestamp}`);
+  try {
+    const response = await fetch(request);
+    cache.put(request, response.clone());
+    return response;
+  } catch (err) {
+    const response = await cache.match(request);
+    if (response)
+      return response;
+    throw err;
+  }
+}
+worker.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || event.request.headers.has("range"))
+    return;
+  const url = new URL(event.request.url);
+  const isHttp = url.protocol.startsWith("http");
+  const isDevServerRequest = url.hostname === self.location.hostname && url.port !== self.location.port;
+  const isStaticAsset = url.host === self.location.host && staticAssets.has(url.pathname);
+  const skipBecauseUncached = event.request.cache === "only-if-cached" && !isStaticAsset;
+  if (isHttp && !isDevServerRequest && !skipBecauseUncached) {
+    event.respondWith((async () => {
+      const cachedAsset = isStaticAsset && await caches.match(event.request);
+      return cachedAsset || fetchAndCache(event.request);
+    })());
+  }
+});
